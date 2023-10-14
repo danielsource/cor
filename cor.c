b6c12e8b63f15e82e65d37133965074171859215
daniel.source <79308609+danielsource@users.noreply.github.com>
2023-10-14 19:58:07 -0300

    Update for feature completeness

--- cor.c
+++ cor.c
@@ -1,49 +1,120 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-/* Comment this out to make the hex triplets uppercase: */
-#define CFG_USE_LOWERCASE
 
 #define CFG_MAX_LINE_LENGTH BUFSIZ
 
+/* Unsigned char (8 bits) */
+typedef unsigned char u8;
+
 typedef enum {
   ERROR_FOPEN = 1,
-  ERROR_LINE_LENGTH = 2,
-  ERROR_FGETS = 4
+  ERROR_INVALID_OPT = 2,
+  ERROR_LINE_LENGTH = 4,
+  ERROR_FGETS = 8
 } exit_status;
 
+/* Hexadecimal triplet color types.
+ * The enum constants use the length as values (e.g. "#0f0" -> length 4). */
 typedef enum {
   HEX_NONE = 0,
-  HEX_RGB,
-  HEX_RGBA,
-  HEX_RRGGBB,
-  HEX_RRGGBBAA
+  HEX_RGB = 4,     /* Ex: #0f0 */
+  HEX_RGBA = 5,    /* Ex: #00f7 */
+  HEX_RRGGBB = 7,  /* Ex: #225577 */
+  HEX_RRGGBBAA = 9 /* Ex: #deadbeef */
 } hex_color_type;
 
-/* Color */
+/* Red, Green and Blue color */
 typedef struct {
-  unsigned char r, g, b;
-} RGB;
+  u8 r, g, b;
+} Rgb;
+
+/* Inserts values into RGB color struct if a hex triplet is
+ * found in the string. Returns the type of the hex triplet. */
+hex_color_type get_hex_color(const char *s, Rgb *clr) {
+  hex_color_type t = HEX_NONE;
+  char rgb_str[3][3];
+
+  if (!isxdigit((u8)s[0]) || !isxdigit((u8)s[1]) || !isxdigit((u8)s[2]))
+    return HEX_NONE;
+  else if (!isalnum((u8)s[3]))
+    t = HEX_RGB;
+  else if (!isxdigit((u8)s[3]))
+    return HEX_NONE;
+  else if (!isalnum((u8)s[4]))
+    t = HEX_RGBA;
+  else if (!isxdigit((u8)s[4]) || !isxdigit((u8)s[5]))
+    t = HEX_NONE;
+  else if (!isalnum((u8)s[6]))
+    t = HEX_RRGGBB;
+  else if (!isxdigit((u8)s[6]) || !isxdigit((u8)s[7]))
+    return HEX_NONE;
+  else if (!isalnum((u8)s[8]))
+    t = HEX_RRGGBBAA;
+  else
+    return HEX_NONE;
+
+  if (t == HEX_RGB || t == HEX_RGBA) {
+    rgb_str[0][0] = s[0];
+    rgb_str[0][1] = s[0];
+    rgb_str[0][2] = '\0';
+
+    rgb_str[1][0] = s[1];
+    rgb_str[1][1] = s[1];
+    rgb_str[1][2] = '\0';
+
+    rgb_str[2][0] = s[2];
+    rgb_str[2][1] = s[2];
+    rgb_str[2][2] = '\0';
+  } else if (t == HEX_RRGGBB || t == HEX_RRGGBBAA) {
+    rgb_str[0][0] = s[0];
+    rgb_str[0][1] = s[1];
+    rgb_str[0][2] = '\0';
+
+    rgb_str[1][0] = s[2];
+    rgb_str[1][1] = s[3];
+    rgb_str[1][2] = '\0';
+
+    rgb_str[2][0] = s[4];
+    rgb_str[2][1] = s[5];
+    rgb_str[2][2] = '\0';
+  }
+
+  clr->r = strtol(rgb_str[0], NULL, 16);
+  clr->g = strtol(rgb_str[1], NULL, 16);
+  clr->b = strtol(rgb_str[2], NULL, 16);
+  return t;
+}
 
 /* Returns whether a RGB color is bright based on HSP
- * (http://alienryderflex.com/hsp.html) */
-int is_color_bright(RGB clr) {
+ * (https://alienryderflex.com/hsp.html) */
+int is_color_bright(Rgb clr) {
   return (clr.r * clr.r * .299f) + (clr.g * clr.g * .587f) +
              (clr.b * clr.b * .114f) >
          127.5f * 127.5f;
 }
 
-hex_color_type get_hex_color(char *s, RGB *clr) { return 0; }
+/* Outputs the hex triplet with a colored background using "true color"
+ * terminal escape codes. Returns the number of chars outputted. */
+int print_hex_color(const char *s, hex_color_type t, Rgb bg) {
+  if (is_color_bright(bg))
+    printf("\033[38;2;0;0;0m\033[48;2;%d;%d;%dm", bg.r, bg.g, bg.b);
+  else
+    printf("\033[38;2;255;255;255m\033[48;2;%d;%d;%dm", bg.r, bg.g, bg.b);
 
-int print_hex_color(hex_color_type t, RGB clr) { return 0; }
+  printf("#%.*s", t - 1, s);
+  fputs("\033[0m", stdout);
+  return t;
+}
 
-/* Outputs the file contents with colored hex triplets (e.g., #f00 with a
- * red background). Returns non-zero in case of error/warning. */
-exit_status cor(FILE *fp) {
+/* Outputs the file contents with colored hex triplets (e.g. #f00 with a
+ * red background). If 'extract' is non-zero only display the colors
+ * instead of the whole file. Returns non-zero in case of error/warning. */
+exit_status cor(FILE *fp, int extract) {
   char line[CFG_MAX_LINE_LENGTH + 2];
+  Rgb clr;
   int i;
-  RGB clr;
   hex_color_type t;
   exit_status sts = 0;
 
@@ -51,33 +122,41 @@ exit_status cor(FILE *fp) {
   clearerr(fp);
   while (fgets(line, sizeof(line), fp)) {
     if (ferror(fp)) {
-      sts |= ERROR_FGETS;
-      return sts;
+      perror("cor: fgets");
+      return sts |= ERROR_FGETS;
     }
 
     if (line[sizeof(line) - 2] != '\0') {
       fprintf(
           stderr,
-          "WARNING: line is as long or longer than the buffer capacity (%lu)\n",
-          sizeof(line) - 1);
+          "cor: WARNING: line is as long or longer than the buffer capacity "
+          "(%d)\n",
+          CFG_MAX_LINE_LENGTH);
       sts |= ERROR_LINE_LENGTH;
       line[sizeof(line) - 2] = '\0';
     }
 
-    if (line[0] == '#' && (t = get_hex_color(&line[1], &clr))) {
-      i = print_hex_color(t, clr);
+    if (line[0] == '#' && (t = get_hex_color(line + 1, &clr))) {
+      i = print_hex_color(line + 1, t, clr);
+      if (extract)
+        putchar('\n');
     } else {
-      putchar(line[0]);
+      if (!extract)
+        putchar(line[0]);
       i = 1;
     }
 
     while (line[i]) {
-      /* TODO: add behind char checking */
-      if (line[i] == '#' && (t = get_hex_color(&line[i + 1], &clr))) {
-        i += print_hex_color(t, clr);
+      if (!isalnum((u8)line[i - 1]) && line[i] == '#' &&
+          (t = get_hex_color(line + i + 1, &clr))) {
+        i += print_hex_color(line + i + 1, t, clr);
+        if (extract)
+          putchar('\n');
         continue;
       }
-      putchar(line[i++]);
+      if (!extract)
+        putchar(line[i]);
+      ++i;
     }
   }
   return sts;
@@ -104,39 +183,52 @@ void test_colors(void) {
     putchar('\n');
   }
 
-  printf("\033[38;2;255;100;0m%s\033[0m\n",
-         "This is orange if true color is supported.");
+  puts("\033[38;2;255;100;0mThis is orange if true color is "
+       "supported.\033[0m");
 }
 
 int main(int argc, char **argv) {
   FILE *fp;
-  int i, use_opts = 1;
+  int i, file_given = 0, use_opts = 1, extract = 0, show_filename = 0;
   exit_status sts = 0;
 
-  if (argc == 1) {
-    return cor(stdin);
-  }
-
   for (i = 1; i < argc; ++i) {
-    if (use_opts) {
-      if (strcmp(argv[i], "--help") == 0) {
-        puts("Usage: cor [--test] [FILE...]");
-        return sts;
-      } else if (strcmp(argv[i], "--test") == 0) {
+    if (use_opts && argv[i][0] == '-') {
+      if (argv[i][1] == '\0') {
+        sts |= cor(stdin, extract);
+        continue;
+      } else if (argv[i][1] == '-' && argv[i][2] == '\0') {
+        use_opts = 0;
+      } else if (strcmp(argv[i] + 2, "extract") == 0) {
+        extract = 1;
+      } else if (strcmp(argv[i] + 2, "show-filename") == 0) {
+        show_filename = 1;
+      } else if (strcmp(argv[i] + 2, "test") == 0) {
         test_colors();
         return sts;
-      } else if (strcmp(argv[i], "--") == 0) {
-        use_opts = 0;
-        continue;
+      } else {
+        puts("Usage: cor [--extract | --show-filename | --test] [FILE...]");
+        if (strcmp(argv[i] + 2, "help") != 0)
+          sts |= ERROR_INVALID_OPT;
+        return sts;
       }
+      continue;
     }
 
+    file_given = 1;
     if (!(fp = fopen(argv[i], "r"))) {
+      fputs("cor: ", stderr);
+      perror(argv[i]);
       sts |= ERROR_FOPEN;
       continue;
     }
-    sts |= cor(fp);
+    if (show_filename)
+      printf("%s:\n", argv[i]);
+    sts |= cor(fp, extract);
     fclose(fp);
   }
+
+  if (!file_given)
+    return cor(stdin, extract);
   return sts;
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
-# cor
+![cor](preview.png "A picture is worth a thousand words")
 
-INSERT IMAGE HERE
+`cor` is similar to the [`cat` command][cat], but outputs the file contents with colored [hexadecimal triplets][hex] (e.g. #f00 with a red background).
 
-`cor` is similar to the [cat command][cat], but outputs the file contents with colored [hexadecimal triplets][hex] (e.g. #f00 with a red background).
+## Build instructions
+
+    $ make
 
 ## How it works?
 
@@ -11,11 +13,19 @@ Using a terminal supporting ["true color"][tc], it is possible to use these [ANS
     ESC[38;2;⟨r⟩;⟨g⟩;⟨b⟩m Select RGB foreground color
     ESC[48;2;⟨r⟩;⟨g⟩;⟨b⟩m Select RGB background color
 
-In practice, this means that you can show the text 'TRUECOLOR' in orange in your terminal this way (assuming "true color" support):
+In practice, this means that you can show the text 'TRUECOLOR' in orange in your terminal supporting "true color" this way:
 
     $ printf '\033[38;2;255;100;0m%s\033[0m\n' TRUECOLOR
+
+## See also
+
+-     $ cor --help
+
+- [ap/vim-css-color][vim-plugin]: A Vim plugin to preview colors in source code while editing.
+  - The inspiration for `cor` (which, by the way, means color in Portuguese)
 
 [cat]: https://en.wikipedia.org/wiki/Cat_(Unix)
 [hex]: https://en.wikipedia.org/wiki/Web_colors#Hex_triplet
 [tc]: https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
 [esc]: https://en.wikipedia.org/wiki/ANSI_escape_code
+[vim-plugin]: https://github.com/ap/vim-css-color
